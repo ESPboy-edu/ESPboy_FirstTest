aa68e2ae7096dacedb4d061bbc81041369241e4c
ESPboy <ESPboy.edu@gmail.com>
2020-10-04 10:14:45 +0300

    refactored

--- ESPboy_FirstTest.ino
+++ ESPboy_FirstTest.ino
@@ -1,75 +1,63 @@
-#include <Adafruit_NeoPixel.h>
-#include <Adafruit_MCP23017.h>
-#include <Adafruit_MCP4725.h>
-#include <Adafruit_ST7735.h> 
-#include <Adafruit_GFX.h> 
-#include <U8x8lib.h>
-#include <Wire.h>   
-#include <ESP8266WiFi.h>
+#include <Adafruit_NeoPixel.h> //to control RGB LED
+#include <Adafruit_MCP23017.h> //to control buttons
+#include <Adafruit_MCP4725.h>  //to control the LCD display backlit
+#include <TFT_eSPI.h>          //to draw at LCD TFT
+#include <ESP8266WiFi.h>       //to control WiFi
+
 #include "ESPboyLogo.h"
 
 #define LEDquantity     1
 #define MCP23017address 0 // actually it's 0x20 but in <Adafruit_MCP23017.h> lib there is (x|0x20) :)
-#define MCP4725dacresolution 8
-#define MCP4725address 0
+#define MCP4725address  0x60
 
 //PINS
 #define LEDPIN         D4
 #define SOUNDPIN       D3
 #define LEDLOCK        9
+#define CSTFTPIN       8 //Chip Select pin for LCD (it's on the MCP23017 GPIO expander GPIO8)
 
+#define PAD_LEFT        0x01
+#define PAD_UP          0x02
+#define PAD_DOWN        0x04
+#define PAD_RIGHT       0x08
+#define PAD_ACT         0x10
+#define PAD_ESC         0x20
+#define PAD_LFT         0x40
+#define PAD_RGT         0x80
+#define PAD_ANY         0xff
 
-//SPI for LCD
-#define csTFTMCP23017pin 8
-#define TFT_RST       -1
-#define TFT_DC        D0
-#define TFT_CS        -1
-
-//FOR OLED
-//U8X8_SSD1306_128X64_NONAME_HW_I2C u8x8(/* reset=*/ U8X8_PIN_NONE); 	      
-U8X8_SH1106_128X64_NONAME_HW_I2C u8x8(/* reset=*/ U8X8_PIN_NONE);
 
 Adafruit_MCP23017 mcp;
-Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_RST);
-Adafruit_NeoPixel pixels(LEDquantity, LEDPIN);
 Adafruit_MCP4725 dac;
+TFT_eSPI tft;
+Adafruit_NeoPixel pixels(LEDquantity, LEDPIN);
+
 
 void setup(){
   Serial.begin(115200); //serial init
-  delay (100);
   WiFi.mode(WIFI_OFF); // to safe some battery power
 
-
-//buttons on mcp23017 init
-  mcp.begin(MCP23017address);
+//DAC init and backlit off
+  dac.begin(MCP4725address);
   delay (100);
+  dac.setVoltage(0, false);
+
+//mcp23017 init for buttons, LED LOCK and TFT Chip Select pins
+  mcp.begin(MCP23017address);
+  delay(100);
+  
   for (int i=0;i<8;i++){  
      mcp.pinMode(i, INPUT);
      mcp.pullUp(i, HIGH);}
-     
+
 //LED init
   mcp.pinMode(LEDLOCK, OUTPUT);
   mcp.digitalWrite(LEDLOCK, HIGH); 
   pinMode(LEDPIN, OUTPUT);
   pixels.begin();
-  delay (100);
+  delay(10);
   pixels.setPixelColor(0, pixels.Color(0,0,0));
   pixels.show();
-
-//TFT init     
-  mcp.pinMode(csTFTMCP23017pin, OUTPUT);
-  mcp.digitalWrite(csTFTMCP23017pin, LOW);
-  tft.initR(INITR_144GREENTAB);
-  delay (100);
-  tft.setRotation(0);
-  tft.fillScreen(ST77XX_BLACK);
-
-//draw ESPboylogo  
-  tft.drawXBitmap(30, 24, ESPboyLogo, 68, 64, ST77XX_YELLOW);
-  tft.setTextSize(1);
-  tft.setTextColor(ST77XX_YELLOW);
-  tft.setCursor(36,102);
-  tft.print ("First test");
 
 //sound init and test
   pinMode(SOUNDPIN, OUTPUT);
@@ -78,77 +66,89 @@ void setup(){
   tone(SOUNDPIN, 100, 100);
   delay(100);
   noTone(SOUNDPIN);
+  
+//LCD TFT init
+  mcp.pinMode(CSTFTPIN, OUTPUT);
+  mcp.digitalWrite(CSTFTPIN, LOW);
+  tft.begin();
+  delay(100);
+  tft.setRotation(0);
+  tft.fillScreen(TFT_BLACK);
 
-//OLED init
-  u8x8.begin();
-  u8x8.setFont(u8x8_font_chroma48medium8_r);  
+//draw ESPboylogo  
+  tft.drawXBitmap(30, 24, ESPboyLogo, 68, 64, TFT_YELLOW);
+  tft.setTextSize(1);
+  tft.setTextColor(TFT_YELLOW);
+  tft.setCursor(36,102);
+  tft.print ("First test");
 
-//DAC init 
-dac.begin(0x60);
-delay (100);
-dac.setVoltage(4095, false);
+//LCD backlit fading on
+  for (uint16_t bcklt=0; bcklt<4095; bcklt+=20){
+    dac.setVoltage(bcklt, false);
+    delay(10);}
 
-//clear TFT
-  delay(2000);
-  tft.fillScreen(ST77XX_BLACK);
+//clear TFT and backlit on high
+  dac.setVoltage(4095, false);
+  tft.fillScreen(TFT_BLACK);
 }
 
 
-void loop(){ 
- uint8_t countled;
- uint8_t static rnd=0;
- uint16_t static lcdbright = 700;
- u8x8.setCursor(0,0);
- countled=0;
- 
- for (int i=0;i<8;i++){
-    if(mcp.digitalRead(i))u8x8.print("-");
-    else {
-       u8x8.print("*");
-       countled++;}
- }
+uint8_t getKeys() { return (~mcp.readGPIOAB() & 255); }
 
- if (countled){      
+
+void loop(){ 
+ uint8_t keypressed;
+ uint8_t static rnd = 0;
+ uint16_t static lcdbright = 700;
+ 
+ keypressed=getKeys();
+
+ if (keypressed){      
+  
     if (rnd==0) {
-      pixels.setPixelColor(0, pixels.Color(countled*20, 0, 0)); 
-      tft.setTextColor(ST77XX_RED); 
-      tft.fillRect(0,100,128,28,ST77XX_RED);}
+      pixels.setPixelColor(0, pixels.Color(100, 0, 0)); 
+      tft.setTextColor(TFT_RED);}
     if (rnd==1) {
-      pixels.setPixelColor(0, pixels.Color(0, countled*20, 0)); 
-      tft.setTextColor(ST77XX_GREEN); 
-      tft.fillRect(0,100,128,28,ST77XX_GREEN);}
+      pixels.setPixelColor(0, pixels.Color(0, 100, 0)); 
+      tft.setTextColor(TFT_GREEN); }
     if (rnd==2) {
-      pixels.setPixelColor(0, pixels.Color(0, 0, countled*20)); 
-      tft.setTextColor(ST77XX_BLUE); 
-      tft.fillRect(0,100,128,28,ST77XX_BLUE);}
-    tft.fillRect(0,0,128,16,ST77XX_BLACK);
-    tft.fillRect(0,64,16,16,ST77XX_BLACK);
-    tft.setCursor(0, 50);
+      pixels.setPixelColor(0, pixels.Color(0, 0, 100)); 
+      tft.setTextColor(TFT_BLUE); }
+      
+    tft.setCursor(0, 0);
     tft.setTextSize(2);
-    tft.println("PRESSED");
-    tft.fillRect(0,64,16,16,ST77XX_BLACK);
-    tft.println(countled);
-    tone(SOUNDPIN, countled*150);
-    lcdbright += 10 * countled;    
-    if (lcdbright > 650) lcdbright = 450;
+    
+    tft.println("PRESSED:");
+    if (keypressed&PAD_LEFT) tft.println("LEFT ");
+    if (keypressed&PAD_RIGHT) tft.println("RIGHT");
+    if (keypressed&PAD_UP) tft.println("UP   ");
+    if (keypressed&PAD_DOWN) tft.println("DOWN ");
+    if (keypressed&PAD_ACT) tft.println("ACT  ");
+    if (keypressed&PAD_ESC) tft.println("ESC  ");
+    if (keypressed&PAD_RGT) tft.println("RGT  ");
+    if (keypressed&PAD_LFT) tft.println("LFT  ");
+    
+    tone(SOUNDPIN, keypressed*30);
+    
+    lcdbright += 30;    
+    if (lcdbright > 1200) lcdbright = 400;
     dac.setVoltage(lcdbright, false);
  }
- else
- { 
+ 
+ else{ 
     rnd=random(0,3);
     noTone(SOUNDPIN);
     pixels.setPixelColor(0, pixels.Color(0, 0, 0)); 
     tft.setCursor(0, 0);
     tft.setTextSize(1);
     tft.print("Millis: "); 
-    tft.fillRect(40,0,70,8,ST77XX_BLACK);
-    tft.fillRect(0,48,120,32,ST77XX_BLACK);
     tft.println(millis());
     tft.setCursor(0, 8);
     tft.print("TFT brt:"); 
-    tft.fillRect(40,8,70,16,ST77XX_BLACK);
     tft.print(lcdbright); 
  }
+ 
  pixels.show();
- delay(100);
+ delay(200);
+ tft.fillScreen(TFT_BLACK);
 }
--- README.md
+++ README.md
@@ -1,11 +1,16 @@
-This source is part of ESPboy.edu project by RomanS
-for first test of ESPboy device after assembling:
+www.espboy.com project
+Try "First test" to check your ESPboy after assembling
 
+Install Arduino IDE https://www.arduino.cc/en/Main/Software
+Install the WeMos D1 mini ESP866 board to your Arduino IDE https://www.instructables.com/Wemos-ESP8266-Getting-Started-Guide-Wemos-101/
+Install all the libs for ESPboy from here https://github.com/ESPboy-edu/ESPboy_ArduinoIDE_Libraries
+
+Select the WeMos D1 mini board in the Arduino IDE and compile this code
+
+Test ESPboy hardware:
 - Power on
-- Uploading this firmware 
-- All 8 buttons
-- Display OLED/TFT
+- 8 buttons
+- Display TFT and its backlit
 - RGB led
 - Buzzer/Speaker
 
-https://www.youtube.com/channel/UCsMjlCb8CK-Cw54lRrkpbQw
